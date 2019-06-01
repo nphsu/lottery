27b7de822206f759cdf2a79aab37ada3c5f00234
shunpei <shunpei42ba@gmail.com>
2019-06-01 23:38:54 +0100

    success buy ticket

--- frontend/pages/luckey_lottery_vr.vue
+++ frontend/pages/luckey_lottery_vr.vue
@@ -5,20 +5,31 @@
         <p>Luckey Lottery</p>
         <p>You can buy a ticket per <span>{{ price }}</span> Ether</p>
         <v-btn @click="buy()">BUY TICKET</v-btn>
-        <v-btn @click="totalQuantity()">TOTAL QUANTITY</v-btn>
       </div>
+    </v-flex>
+    <v-flex md12>
+      <a-scene>
+        <a-sky color="#212121">
+        </a-sky>
+      </a-scene>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import 'aframe'
+import 'aframe-event-set-component'
 import Web3 from 'web3'
+import Tx from 'ethereumjs-tx'
 import luckeyLotteryJSON from '~/contracts/LuckeyLottery.json'
-const web3 = new Web3(Web3.givenProvider)
+const web3 = new Web3(window.web3.currentProvider)
 const luckeyLotteryABI = luckeyLotteryJSON.abi
 const luckeyLotteryAddress = luckeyLotteryJSON.networks[5777].address
 const luckeyLottery = web3.eth.Contract(luckeyLotteryABI, luckeyLotteryAddress)
-const playerAddress = window.web3.eth.accounts[0]
+const ownerPrivKey = process.env.OWNER_PRIV_KEY
+console.log(ownerPrivKey) // Why don't I get this..
+// const playerAddress = web3.eth.accounts[0] // Why don't I get this..
+const playerAddress = '0xC2B9Da58dF9Be7307781e8C363Ec96958e8943D5' // will remove
 
 export default {
   components: {},
@@ -29,25 +40,21 @@ export default {
   },
   created: async function () {
     const price = await luckeyLottery.methods.getPrice().call()
-    if (price === undefined || price ==='') {
-      this.price = 0
-    }
-    this.price = price.toNumber() / 1e18
+    const priceEther = price.toNumber() / 1e18
+    this.price = priceEther;
   },
   methods: {
     getPrice: function () {
       return this.price
     },
     buy: async function () {
-      console.log(window.web3.eth.accounts)
       const encodedData = luckeyLottery.methods.buy().encodeABI()
       const txCount = await this.getTxCount(playerAddress)
       const rowTx = this.makeRowTx(encodedData, txCount)
       console.log(rowTx)
-      window.web3.eth.sendTransaction(rowTx, (e, id) => {
-        console.log(e)
-        console.log(id)
-      })
+      const signedTx = this.signTx(rowTx)
+      console.log(signedTx)
+      await this.sendTx(signedTx)
       console.log('buy')
     },
     getTxCount: async address => {
@@ -55,34 +62,28 @@ export default {
       return count
     },
     makeRowTx: (encodedData, txCount) => {
-      const value = 0.001 * 1000000000000000000
       return {
         from: playerAddress,
         to: luckeyLotteryAddress,
         gasPrice: web3.utils.toHex(3 * 1e10),
         gasLimit: web3.utils.toHex(5000000),
         nonce: '0x' + txCount.toString(16),
-        value: value,
+        value: '0x0',
         data: encodedData,
         chainId: 42 // Why 42
       }
     },
-    // signTx: rowTx => {
-    //   const privKey = Buffer.from(ownerPrivKey, 'hex')
-    //   const tx = new Tx(rowTx)
-    //   tx.sign(privKey)
-    //   return tx
-    // },
-    // sendTx: async tx => {
-    //   const serializedTx = tx.serialize()
-    //   await web3.eth
-    //   .sendSignedTransaction('0x' + serializedTx.toString('hex'))
-    //   .on('receipt', console.log)
-    // }
-    totalQuantity: async () => {
-      const quantity = await luckeyLottery.methods.getTotalQuantity().call()
-      console.log(quantity.toNumber() / 1e18)
-      return quantity
+    signTx: rowTx => {
+      const privKey = Buffer.from(ownerPrivKey, 'hex')
+      const tx = new Tx(rowTx)
+      tx.sign(privKey)
+      return tx
+    },
+    sendTx: async tx => {
+      const serializedTx = tx.serialize()
+      await web3.eth
+      .sendSignedTransaction('0x' + serializedTx.toString('hex'))
+      .on('receipt', console.log)
     }
   }
 }
