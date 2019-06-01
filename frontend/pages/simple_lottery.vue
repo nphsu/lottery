bf77321d1515c5f36f1fe52af67adcc1383ce0c4
shunpei <shunpei42ba@gmail.com>
2019-06-01 20:04:41 +0100

    add contract

--- frontend/pages/simple_lottery.vue
+++ frontend/pages/simple_lottery.vue
@@ -3,7 +3,8 @@
     <v-flex>
       <div class="text-xs-center">
         <p>Simple Lottery</p>
-        <button @click="getTicketPrice()">aaa</button>
+        <v-btn @click="getTicketPrice()">GetName</v-btn>
+        <v-btn @click="getRand()">GetRand</v-btn>
       </div>
     </v-flex>
   </v-layout>
@@ -15,21 +16,21 @@ import simpleLotteryJSON from '~/contracts/SimpleLottery.json'
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 const simpleLotteryABI = simpleLotteryJSON.abi
 const simpleLotteryAddress = simpleLotteryJSON.networks[5777].address
-
-
+const simpleLottery = web3.eth.Contract(simpleLotteryABI, simpleLotteryAddress)
+      
 export default {
   components: {},
+  
   methods: {
     getTicketPrice: async function() {
-      const simpleLottery = web3.eth.Contract(simpleLotteryABI, simpleLotteryAddress)
-      console.log(simpleLottery)
-      // await simpleLottery.methods.getRand().call()
       const showingAccounts = await web3.eth.getAccounts()
       console.log(showingAccounts)
       const name = await simpleLottery.methods.getName().call()
       console.log(name)
-      // const rand = await simpleLottery.methods.getRand().call()
-      // console.log(rand)
+    },
+    getRand: async function() {
+      const rand = await simpleLottery.methods.getRand().call()
+      console.log(rand)
     }
   }
 }
