8ed937a5840334c330ad94cfc87efff7dbde717d
shunpei <shunpei42ba@gmail.com>
2019-06-08 18:49:55 +0100

    add terms of service page

---
+++ frontend/pages/tos.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-layout>
+    <v-flex md12>
+      <div class="text-xs-center">
+        <p class="display-2">Terms of Service</p>
+      </div>
+      <v-divider/>
+      <div class="text-xs-left">
+        <p><span class="headline font-weight-bold font-italic">1. </span>The Intellectual Property disclosure will inform users that the contents, logo and other visual media you created is your property and is protected by copyright laws.</p>
+        <p><span class="headline font-weight-bold font-italic">2. </span>A Termination clause will inform that users’ accounts on your website and mobile app or users’ access to your website and mobile (if users can’t have an account with you) can be terminated in case of abuses or at your sole discretion.</p>
+        <p><span class="headline font-weight-bold font-italic">3. </span>A Governing Law will inform users which laws govern the agreement. This should the country in which your company is headquartered or the country from which you operate your website and mobile app.</p>
+        <p><span class="headline font-weight-bold font-italic">4. </span>A Links To Other Web Sites clause will inform users that you are not responsible for any third party websites that you link to. This kind of clause will generally inform users that they are responsible for reading and agreeing (or disagreeing) with the Terms and Conditions or Privacy Policies of these third parties.</p>
+        <p><span class="headline font-weight-bold font-italic">5. </span>If your website or mobile app allows users to create content and make that content public to other users, a Content section will inform users that they own the rights to the content they have created. The “Content” clause usually mentions that users must give you (the website or mobile app developer) a license so that you can share this content on your website/mobile app and to make it available to other users.</p>
+      </div>
+      <v-divider/>
+      <div class="text-xs-center">
+        <v-btn @click="$router.push(`/`)">BACK TO TOP</v-btn>    
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
